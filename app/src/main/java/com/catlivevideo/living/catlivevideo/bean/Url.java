2b5a6ffb9fa0612c7b2815e343ea01ead33ddc4d
panmaoyun008@163.com <panamoyun008@163.com>
2016-10-24 21:30:56 +0800

    init

--- app/src/main/java/com/catlivevideo/living/catlivevideo/bean/Url.java
+++
@@ -1,8 +0,0 @@
-package com.catlivevideo.living.catlivevideo.bean;
-public class Url {
-	public static final String URL = "http://5minutesbinary.com:8080/CloudLiveVideoServer/";
-	public static final String REGISTE_PATH = URL+"user?flag=register";
-	public static final String LOGIN_PATH = URL+"user?flag=login";
-	public static final String GET_LIVE_COUNT = URL+"livecount";
-	public static final String SET_ISLIVE = URL+"setlive";
-}
--- app/src/main/libenc/jni/libyuv/jni/Application.mk
+++
@@ -1,2 +0,0 @@
-APP_ABI := armeabi armeabi-v7a x86 mips x86_64 mips64 arm64-v8a
-APP_PLATFORM := android-L
